bd4980cf4d822b311c1c06e7d95793953817eb55
tovdata <dev@tovdata.com>
2022-01-25 18:24:36 +0900

    2022.01.25. - Implement class for amazon s3 bucket - Implement class for aws apigateway for rest api

---
+++ resources/apigateway .ts
@@ -0,0 +1,255 @@
+import { Construct } from "constructs";
+import { aws_apigateway as apigateway } from "aws-cdk-lib";
+// Util
+import { storeResource } from "../utils/cache";
+import { createId, extractTags } from "../utils/util";
+
+export class RestApi {
+  private _mapping: any;
+  private _restApi: apigateway.CfnRestApi;
+  private _scope: Construct;
+
+  /**
+   * Create the rest api
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-apigateway-restapi.html
+   * @param scope scope context
+   * @param config configuration for rest api
+   */
+  constructor(scope: Construct, config: any) {
+    this._scope = scope;
+    this._mapping = { authorizer: {}, method: {}, model: {}, resource: {}, requestValidator: {} };
+    // Extract a list of tag
+    const tags = extractTags(config.tags);
+    // Set the properties for rest api
+    const props: apigateway.CfnRestApiProps = {
+      apiKeySourceType: config.apiKeySource,
+      description: config.description,
+      disableExecuteApiEndpoint: config.disableExecuteApiEndpoint,
+      endpointConfiguration: config.endpointConfiguration,
+      name: config.name,
+      tags: tags.length > 0 ? tags : undefined
+    };
+    // Create the rest api
+    this._restApi = new apigateway.CfnRestApi(this._scope, createId(JSON.stringify(props)), props);
+    // Store the resource
+    storeResource("apigateway", config.name, this._restApi);
+  }
+
+  /**
+   * Create the authorizer
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-apigateway-authorizer.html
+   * @param config configuration for authorizer
+   */
+  public createAuthorizer(config: any): void {
+    // Create the properties for authorizer
+    const props: apigateway.CfnAuthorizerProps = {
+      authorizerCredentials: config.authorizerCredentials,
+      authorizerResultTtlInSeconds: config.authorizerResultTtlInSeconds !== undefined ? Number(config.authorizerResultTtlInSeconds) : undefined,
+      authorizerUri: config.authorizerUri,
+      authType: config.authType,
+      identitySource: config.identitySource,
+      identityValidationExpression: config.identityValidationExpression,
+      name: config.name,
+      restApiId: this._restApi.ref,
+      type: config.type
+    };
+    // Create the authorizer
+    const authorizer = new apigateway.CfnAuthorizer(this._scope, createId(JSON.stringify(props)), props);
+    // Store mapping resource
+    this._mapping.authorizer[config.id] = authorizer.ref;
+  }
+
+  /**
+   * Create the resource methods
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-apigateway-method.html
+   * @param configs configuration for method
+   */
+  public createMethod(path: string, config: any): void {
+    const resourceId = this._mapping.resource[path];
+    for (const methodType of Object.keys(config)) {
+      const methodOptions = config[methodType];
+      // Set the request models in method
+      const requestModels: any = {};
+      if (methodOptions.requestModels !== undefined) {
+        Object.entries(methodOptions.requestModels).forEach((elem: any) => requestModels[elem.key]=elem.value);
+      }
+      // Set the method responses
+      const methodResponses: apigateway.CfnMethod.MethodResponseProperty[] = methodOptions.methodResponses !== undefined ? Object.keys(methodOptions.methodResponses).map((key: string): apigateway.CfnMethod.MethodResponseProperty => {
+        const value: any = methodOptions.methodResponses[key];
+        // Set the request models in method responses
+        const requestModels: any = {};
+        if (value.requestModels !== undefined) {
+          Object.entries(value.requestModels).forEach((elem: any) => requestModels[elem.key]=elem.value);
+        }
+        // Return
+        return {
+          statusCode: value.statusCode,
+          responseParameters: value.responseParameters,
+          responseModels: Object.keys(requestModels).length > 0 ? requestModels : undefined,
+        }
+      }) : [];
+      // Set the properties for resource method [Ref. https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-apigateway-method.html]
+      const props: apigateway.CfnMethodProps = {
+        apiKeyRequired: methodOptions.apiKeyRequired,
+        httpMethod: methodOptions.httpMethod,
+        methodResponses: methodResponses.length > 0 ? methodResponses : undefined,
+        resourceId: resourceId,
+        restApiId: this._restApi.ref,
+        requestModels: Object.keys(requestModels).length > 0 ? requestModels : undefined,
+        requestParameters: methodOptions.requestParameters !== undefined ? Object.keys(methodOptions.requestParameters).length > 0 ? methodOptions.requestParameters : undefined : undefined,
+        requestValidatorId: methodOptions.requestValidatorId !== undefined ? this._mapping.requestValidator[methodOptions.requestValidatorId] : undefined
+      };
+      // Create the resource method
+      this._mapping.method[`${path}:${methodOptions.httpMethod}`] = new apigateway.CfnMethod(this._scope, createId(JSON.stringify(props)), props);
+    }
+  }
+
+  /**
+   * Create the  model
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-apigateway-model.html
+   * @param config configuration for model
+   */
+  public createModel(config: any): void {
+    if (config.name !== "Empty" && config.name !== "Error") {
+      // Create the properties for model
+      const props: apigateway.CfnModelProps = {
+        contentType: config.contentType,
+        description: config.description,
+        name: config.name,
+        restApiId: this._restApi.ref,
+        schema: config.schema !== undefined ? JSON.parse(config.schema) : undefined
+      };
+      // Create the model
+      const model = new apigateway.CfnModel(this._scope, createId(JSON.stringify(props)), props);
+      // Store mpping resource
+      this._mapping.model[config.name] = model.ref;
+    }
+  }
+
+  /**
+   * Create the gateway response
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-apigateway-gatewayresponse.html
+   * @param config configuration for gateway response
+   */
+  public createGatewayResponse(config: any): void {
+    // Create the properties for gateway response
+    const props: apigateway.CfnGatewayResponseProps = {
+      responseType: config.responseType,
+      restApiId: this._restApi.ref,
+      responseParameters: config.responseParameters !== undefined ? Object.keys(config.responseParameters).length > 0 ? config.responseParameters : undefined : undefined,
+      responseTemplates: config.responseTemplates !== undefined ? Object.keys(config.responseTemplates).length > 0 ? config.responseTemplates : undefined : undefined,
+      statusCode: config.statusCode
+    };
+    // Create the gateway response
+    const gatewayResponse = new apigateway.CfnGatewayResponse(this._scope, createId(JSON.stringify(props)), props);
+  }
+
+  /**
+   * Create the resource in rest api
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-apigateway-resource.html
+   * @param parentId parent resource id
+   * @param resourcePath resource path
+   * @param pathPart path part
+   */
+  public createResource(parentId: string, resourcePath: string, pathPart: string): void {
+    // Create the properties for resource in rest api
+    const props: apigateway.CfnResourceProps = {
+      parentId: parentId,
+      pathPart: pathPart,
+      restApiId: this._restApi.ref
+    };
+    // Create the resource in rest api
+    const resource = new apigateway.CfnResource(this._scope, createId(JSON.stringify(props)), props);
+    // Store mapping resource
+    this._mapping.resource[resourcePath] = resource.ref;
+  }
+
+  /**
+   * Create the resource in rest api using path tree
+   * @param parentId parent resource id
+   * @param resourcePath resource path
+   * @param tree tree for resource path
+   */
+  private createResourceUsingTree(parentId: string, resourcePath: string, tree: any): void {
+    for (const key of Object.keys(tree)) {
+      // Create the resource path
+      const path: string = `${resourcePath}/${key}`;
+      // Set properties for resource in rest api [Ref. https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-apigateway-resource.html]
+      const props: apigateway.CfnResourceProps = {
+        parentId: parentId,
+        pathPart: key,
+        restApiId: this._restApi.ref
+      };
+      // Create the resource in rest api
+      const resource = new apigateway.CfnResource(this._scope, createId(JSON.stringify(props)), props);
+      this._mapping.resource[path] = resource.ref;
+      // If you have a child node, execute a recursive function.
+      if (tree[key] !== undefined) {
+        this.createResourceUsingTree(resource.ref, path, tree[key]);
+      }
+    }
+  }
+
+  /**
+   * Create the resources in rest api
+   * @param config configuration for resources
+   */
+  public createResources(config: any): void {
+    // Create the path tree based on resource path
+    const tree: any = this.extractPathTree(config);
+    // Create the resources
+    this.createResourceUsingTree(this._restApi.attrRootResourceId, "", tree[""]);
+  }
+
+  /**
+   * Create the request validator
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-apigateway-requestvalidator.html
+   * @param config configuration for request validator
+   */
+  public createRequestValidator(config: any): void {
+    // Create the properties for request validator
+    const props: apigateway.CfnRequestValidatorProps = {
+      name: config.name,
+      restApiId: this._restApi.ref,
+      validateRequestBody: config.validateRequestBody,
+      validateRequestParameters: config.validateRequestParameters
+    };
+    // Create the request validator
+    const requestValidator = new apigateway.CfnRequestValidator(this._scope, createId(JSON.stringify(props)), props);
+    // Store mapping resource
+    this._mapping.requestValidator[config.id] = requestValidator.ref;
+  }
+
+  /**
+   * Extract the tree for resource path
+   * @param configs configuration for resources
+   * @returns tree for resource path
+   */
+  public extractPathTree(configs: any[]): any {
+    // Extract tree for path
+    const tree: any = {};
+    for (const config of configs) {
+      let parent: any = null;
+      const paths: string[] = config.path.split("/");
+      for (const path of paths) {
+        if (parent === null) {
+          if (tree[path] === undefined) tree[path] = {};
+          parent = tree[path];
+        } else {
+          if (path !== "" && parent[path] === undefined) parent[path] = {};
+          parent = parent[path];
+        }
+      }
+    }
+    // Return
+    return tree;
+  }
+
+  /**
+   * Get id for bucket
+   * @returns id for bucket
+   */
+  public getId(): string {
+    return this._restApi.ref;
+  }
+}
---
+++ resources/s3.ts
@@ -0,0 +1,302 @@
+import { Construct } from "constructs";
+import { aws_s3 as s3 } from "aws-cdk-lib";
+// Util
+import { getResource, storeResource } from "../utils/cache";
+import { createId } from "../utils/util";
+
+export class Bucket {
+  private _bucket: s3.CfnBucket;
+  private _scope: Construct;
+
+  /**
+   * Create the s3 bucket
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-s3-bucket.html
+   * @param scope scope context
+   * @param config configuration for bucket
+   */
+  constructor(scope: Construct, config: any) {
+    this._scope = scope;
+
+    // Set the properties for bucket
+    const props: s3.CfnBucketProps = {
+      accelerateConfiguration: config.AccelerateConfiguration !== undefined ? { accelerationStatus: config.AccelerateConfiguration.AccelerationStatus } : undefined,
+      bucketName: config.Name,
+    };
+    // Create the bucket
+    this._bucket = new s3.CfnBucket(this._scope, createId(JSON.stringify(props)), props);
+    // Store the resource
+    storeResource("s3", config.Name, this._bucket);
+  }
+
+  /**
+   * Extract the notification filter rules
+   * @param config configuration for notification filter rules
+   * @returns notification filter rules
+   */
+  private extractNotificationFilterRules(config: any): s3.CfnBucket.FilterRuleProperty[] {
+    return config.Key !== undefined ? config.Key.FilterRules ? config.Key.FilterRules.map((rule: any): s3.CfnBucket.FilterRuleProperty => { return { name: rule.Name, value: rule.Value }; }) : [] : [];
+  }
+
+  /**
+   * Get an arn for bucket
+   * @returns arn for bucket
+   */
+  private getArn(): string {
+    return this._bucket.attrArn;
+  }
+
+  /**
+   * Get a mapping arn for lambda function
+   * @param prevArn previous arn for lambda function
+   * @returns arn for lambda function
+   */
+  private getMappingLambdaFunctionArn(prevArn: string): string {
+    const arnSplit: string[] = prevArn.split(":");
+    // Process according to split length
+    let functionName: string = "";
+    let versionOrAlias: string = "";
+    if (arnSplit.length === 7) {
+      functionName = arnSplit[arnSplit.length - 1];
+    } else if (arnSplit.length === 8) {
+      functionName = arnSplit[arnSplit.length - 2];
+      versionOrAlias = arnSplit[arnSplit.length - 1];
+    }
+    // Get a lambda function
+    const lambdaFunction: any = getResource("lambda", functionName);
+    if (lambdaFunction !== undefined) {
+      if (versionOrAlias !== "") return lambdaFunction.getArn();
+      else return `${lambdaFunction.getArn()}:${versionOrAlias}`;
+    } else {
+      return prevArn;
+    }
+  }
+
+  /**
+   * Get a mapping arn for topic
+   * @param prevArn previous arn for topic
+   * @returns arn for topic
+   */
+   private getMappingTopicArn(prevArn: string): string {
+    const arnSplit: string[] = prevArn.split(":");
+    if (arnSplit.length === 6) {
+      const queue: any = getResource("sns", arnSplit[arnSplit.length - 1]);
+      if (queue !== undefined) {
+        return queue.getArn();
+      } else {
+        return prevArn;
+      }
+    }
+  }
+
+  /**
+   * Get a mapping arn for queue
+   * @param prevArn previous arn for queue
+   * @returns arn for queue
+   */
+  private getMappingQueueArn(prevArn: string): string {
+    const arnSplit: string[] = prevArn.split(":");
+    if (arnSplit.length === 6) {
+      const queue: any = getResource("sqs", arnSplit[arnSplit.length - 1]);
+      if (queue !== undefined) {
+        return queue.getArn();
+      } else {
+        return prevArn;
+      }
+    }
+  }
+
+  /**
+   * Get a name for bucket
+   * @returns name for bucket
+   */
+  private getName(): string {
+    return this._bucket.ref;
+  }
+
+  /**
+   * Set the CORS for bucket
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-corsrule.html
+   * @param config configuration for CORS
+   */
+  public setCorsConfiguration(config: any): void {
+    // Create the cors rules
+    const corsRoles: any[] = config.CorsConfiguration !== undefined ? config.CorsConfiguration.CORSRules.map((elem: any): s3.CfnBucket.CorsRuleProperty => {
+      return {
+        allowedMethods: elem.AllowedMethods,
+        allowedOrigins: elem.AllowedOrigins,
+        allowedHeaders: elem.AllowedHeaders,
+        exposedHeaders: elem.ExposedHeaders,
+        maxAge: elem.MaxAgeSeconds ? Number(elem.MaxAgeSeconds) : undefined
+      };
+    }) : [];
+    // Set the cors configuration
+    if (corsRoles.length > 0) {
+      this._bucket.addPropertyOverride("CorsConfiguration", { corsRoles: corsRoles });
+    }
+  }
+
+  /**
+   * Set the logging for bucket
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-loggingconfiguration.html
+   * @param config configuration for logging
+   */
+  public setLogging(config: any): void {
+    // Set the logging configuration
+    if (config !== undefined && config !== null) {
+      this._bucket.addPropertyOverride("LoggingConfiguration", {
+        destinationBucketName: config.TargetBucket,
+        logFilePrefix: config.TargetPrefix
+      });
+    }
+  }
+
+  /**
+   * Set the notifications for bucket
+   * @param config configuration for notifications
+   */
+  public setNotifications(config: any): void {
+    if (config !== undefined && Object.keys(config).length > 0) {
+      let eventBridgeConfigurations: s3.CfnBucket.EventBridgeConfigurationProperty = {};
+      const lambdaConfigurations: s3.CfnBucket.LambdaConfigurationProperty[] = [];
+      const topicConfigurations: s3.CfnBucket.TopicConfigurationProperty[] = [];
+      const queueConfigurations: s3.CfnBucket.QueueConfigurationProperty[] = [];
+      // Set the notification configuration for eventbridge
+      if (config.EventBridgeConfigurations !== undefined) {
+        eventBridgeConfigurations = config.EventBridgeConfigurations;
+      }
+      // Set the notification configuration for lambda functions
+      if (config.LambdaFunctionConfigurations !== undefined && config.LambdaFunctionConfigurations.length > 0) {
+        for (const elem of config.LambdaFunctionConfigurations) {
+          // Set the notification filter rules
+          const rules: any[] = this.extractNotificationFilterRules(elem);
+          // Set the notification for lambda function
+          for (const event of elem.Events) {
+            lambdaConfigurations.push({
+              event: event,
+              function: this.getMappingLambdaFunctionArn(elem.LambdaFunctionArn),
+              filter: {
+                s3Key: { rules }
+              }
+            });
+          }
+        }
+      }
+      // Set the notification configuration for topic
+      if (config.TopicConfigurations !== undefined && config.TopicConfigurations.length > 0) {
+        for (const elem of config.TopicConfigurations) {
+          // Extract the topic arn
+          const topicArn: string = this.getMappingTopicArn(elem.Name);
+          // Set the notification filter rules
+          const rules: any[] = this.extractNotificationFilterRules(elem);
+          // Set the notification for topic
+          for (const event of elem.Events) {
+            topicConfigurations.push({
+              event: event,
+              topic: topicArn,
+              filter: {
+                s3Key: { rules }
+              }
+            });
+          }
+        }
+      }
+      // Set the notification configuration for queue
+      if (config.QueueConfigurations !== undefined && config.QueueConfigurations.length > 0) {
+        for (const elem of config.QueueConfigurations) {
+          // Set the notification filter rules
+          const rules: any[] = this.extractNotificationFilterRules(elem);
+          // Set the notification for queue
+          for (const event of elem.Events) {
+            queueConfigurations.push({
+              event: event,
+              queue: this.getMappingQueueArn(elem.Name),
+              filter: {
+                s3Key: { rules }
+              }
+            });
+          }
+        }
+      }
+
+      // Set the properties for notification
+      const props: s3.CfnBucket.NotificationConfigurationProperty = {
+        eventBridgeConfiguration: Object.keys(eventBridgeConfigurations).length > 0 ? eventBridgeConfigurations : undefined,
+        lambdaConfigurations: lambdaConfigurations.length > 0 ? lambdaConfigurations : undefined,
+        topicConfigurations: topicConfigurations.length > 0 ? topicConfigurations : undefined,
+        queueConfigurations: queueConfigurations.length > 0 ? queueConfigurations : undefined
+      };
+      // Set the notification for bucket
+      this._bucket.addPropertyOverride("NotificationConfiguration", props);
+    }
+  }
+
+  /**
+   * Set the ownership controls for bucket
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-ownershipcontrols.html
+   * @param config configuration for ownership controls
+   */
+  public setOwnershipControls(config: any): void {
+    if (config !== undefined) {
+      // Set the ownership control rules
+      const rules: string[] = config.Rules.map((elem: any): s3.CfnBucket.OwnershipControlsRuleProperty => { return { objectOwnership: elem.ObjectOwnership }; });
+      // Set the ownership controls
+      if (rules.length > 0) {
+        this._bucket.addPropertyOverride("OwnershipControls", { rules: rules });
+      }
+    }
+  }
+
+  /**
+   * Set the public access block for bucket
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-publicaccessblockconfiguration.html
+   * @param config configuration for public access block
+   */
+  public setPublicAccessBlock(config: any): void {
+    if (config !== undefined) {
+      // Set the properties for public access block
+      const props: s3.CfnBucket.PublicAccessBlockConfigurationProperty = {
+        blockPublicAcls: config.BlockPublicAcls,
+        blockPublicPolicy: config.BlockPublicPolicy,
+        ignorePublicAcls: config.IgnorePublicAcls,
+        restrictPublicBuckets: config.RestrictPublicBuckets
+      };
+      // Set the public access block
+      this._bucket.addPropertyOverride("PublicAccessBlockConfiguration", props);
+    }
+  }
+
+  /**
+   * Set the website for bucket
+   * @description https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-websiteconfiguration.html
+   * @param config configuration for website
+   */
+  public setWebsite(config: any): void {
+    if (config !== undefined) {
+      // Extract the routes
+      const routes: any[] = config.RoutingRules !== undefined ? config.RoutingRules.map((elem: any) => {
+        return {
+          redirectRule: elem.RoutingRuleCondition !== undefined ? {
+            hostName: elem.RoutingRuleCondition.HostName,
+            httpRedirectCode: elem.RoutingRuleCondition.HttpRedirectCode,
+            protocol: elem.RoutingRuleCondition.Protocol,
+            replaceKeyPrefixWith: elem.RoutingRuleCondition.ReplaceKeyPrefixWith,
+            replaceKeyWith: elem.RoutingRuleCondition.ReplaceKeyWith
+          } : undefined,
+          routingRuleCondition: elem.RoutingRuleCondition !== undefined ? {
+            httpErrorCodeReturnedEquals: elem.RoutingRuleCondition.HttpErrorCodeReturnedEquals,
+            keyPrefixEquals: elem.RoutingRuleCondition.KeyPrefixEquals
+          }: undefined
+        }
+      }) : [];
+
+      // Set the properties for website configuration
+      const props: s3.CfnBucket.WebsiteConfigurationProperty = {
+        errorDocument: config.ErrorDocument !== undefined ? config.ErrorDocument : undefined,
+        indexDocument: config.IndexDocument !== undefined ? config.IndexDocument.Suffix : undefined,
+        routingRules: routes.length > 0 ? routes : undefined
+      };
+      // Set the website configuration
+      this._bucket.addPropertyOverride("WebsiteConfiguration", props);
+    }
+  }
+}
