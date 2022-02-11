886291448737cac001e850c3ad035a092b8bcb6b
hmin01 <goals1003@gmail.com>
2022-02-11 19:11:09 +0900

    2022.02.11. - Implement function to put the method integration (contain response) and the method responses using awd sdk - Implement function to deploy the stage for rest api using aws sdk - Implement function to set method options (integration, responses) in sdk part - Implement function to deploy the stage for rest api in sdk part

--- lib/sdk/index.ts
+++ lib/sdk/index.ts
@@ -25,20 +25,64 @@ export function initSdkClients() {
 }
 
 /** For APIGateway */
+/** For APIGateway */
 /**
  * Set the method integrations
  * @param name api name
  * @param config configuration for api
  */
-export async function setAPIGatewayMethodIntegrations(name: string, config: any[]): Promise<void> {
+export async function setAPIGatewayMethods(name: string, config: any[]): Promise<void> {
+  // Get an api id
+  const restApiId: string = await SDKAPIGateway.getRestApiId(name);
+  if (restApiId === "") {
+    console.error(`[ERROR] Not found api id (for ${name})`);
+    process.exit(1);
+  }
+  // Set the methods
   for (const elem of config) {
+    // Get a resource id
+    const resourceId: string = await SDKAPIGateway.getResourceId(restApiId, elem.path);
+    if (resourceId === "") {
+      console.error(`[ERROR] Not found resource id (for ${elem.path})`);
+      break;
+    }
+    // Set a method
     if (elem.resourceMethods !== undefined) {
       for (const method of Object.keys(elem.resourceMethods)) {
-        if (elem.resourceMethods[method].methodIntegration !== undefined) {
-          await SDKAPIGateway.putMethodIntegration(name, elem.path, method, elem.resourceMethods[method].methodIntegration);
-        }
+        const configForIntegration: any = elem.resourceMethods[method].methodIntegration;
+        const configForResponse: any = elem.resourceMethods[method].methodResponses;
+        // Put the method integration
+        await SDKAPIGateway.putMethodIntegration(restApiId, resourceId, method, configForIntegration);
+        // Put the method response
+        await SDKAPIGateway.putMethodResponse(restApiId, resourceId, method, configForResponse);
+        // Put the method integration response
+        await SDKAPIGateway.putMethodIntegrationResponse(restApiId, resourceId, method, configForIntegration !== undefined ? configForIntegration.integrationResponses : undefined);
+        // Print console
+        console.info(`[NOTICE] Put the method (for ${method} ${elem.path})`);
       }
     } 
+  }
+}
+/**
+ * Deploy the API Gateway stage (contain deployment)
+ * @param name rest api name
+ * @param config configuration for stage
+ */
+export async function deployAPIGatewayStage(name: string, config: any[]): Promise<void> {
+  // Get an api id
+  const restApiId: string = await SDKAPIGateway.getRestApiId(name);
+  if (restApiId === "") {
+    console.error(`[ERROR] Not found api id (for ${name})`);
+    process.exit(1);
+  }
+  // Process
+  for (const elem of config) {
+    // Create the deployment
+    const deployment: string = await SDKAPIGateway.createDeployment(restApiId);
+    // Create the stage
+    await SDKAPIGateway.createStage(restApiId, deployment, elem);
+    // Print message
+    console.info(`[NOTICE] Deploy the stage (for ${name})`);
   }
 }
 
@@ -61,7 +105,7 @@ export async function setAPIGatewayMethodIntegrations(name: string, config: any[
  * Set the event source mappings
  * @param config configuration for event source mappings
  */
- export async function setEventSourceMappings(config: any): Promise<void> {
+export async function setEventSourceMappings(config: any): Promise<void> {
   for (const mappingId of Object.keys(config)) {
     await SDKLambda.setEventSourceMapping(config[mappingId]);
   }
