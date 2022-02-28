f1d1704b1144366c53989c051f0071a88b4a40e2
hmin01 <goals1003@gmail.com>
2022-02-28 12:11:32 +0900

    2022.02.28. - Fixed a bug (in cognito class in cdk)

--- dist/cdk/services/cognito.js
+++ dist/cdk/services/cognito.js
@@ -18,7 +18,7 @@ class UserPool {
         const schema = [];
         if (config.SchemaAttributes) {
             for (const elem of config.SchemaAttributes) {
-                if (!defaultSchema[elem.Name] || elem.Mutable || elem.Required) {
+                if ((!defaultSchema[elem.Name] || elem.Required) && elem.Name.length <= 20) {
                     schema.push({
                         attributeDataType: elem.AttributeDataType,
                         developerOnlyAttribute: elem.DeveloperOnlyAttribute,
